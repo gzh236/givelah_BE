8e5abc2e7eac20d6ceb2bd500d54331f6206b35d
gzh236 <47617174+gzh236@users.noreply.github.com>
2021-10-25 16:29:32 +0800

    #2 refactor firebase creds

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,29 +2,26 @@ require("dotenv").config();
 
 // const serviceAccount = require("../../google-credentials.json");
 
-import { initializeApp } from "firebase/app";
 import * as admin from "firebase-admin";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
-  private_key: process.env.PRIVATE_KEY,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY!,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+//   projectId: process.env.FIREBASE_PROJECT_ID!,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+//   appId: process.env.FIREBASE_APP_ID!,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
+//   private_key: process.env.PRIVATE_KEY,
+// };
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: process.env.PRIVATE_KEY!.replace(/\\n/g, "\n"),
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
   }),
-  databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`,
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
 });
-
-const app = initializeApp(firebaseConfig);
 
 export default admin;
