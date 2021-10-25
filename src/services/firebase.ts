d4f715d85caa4d9cc2fccca13820840ad88d0ec9
gzh236 <47617174+gzh236@users.noreply.github.com>
2021-10-25 15:12:24 +0800

    attempt1 to fix firebase credentials

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,23 +1,23 @@
 require("dotenv").config();
 
-const serviceAccount = require("../../google-credentials.json");
+// const serviceAccount = require("../../google-credentials.json");
 
 import { initializeApp } from "firebase/app";
 import * as admin from "firebase-admin";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+  credential: admin.credential.cert(firebaseConfig),
+  databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`,
 });
 
 initializeApp(firebaseConfig);
